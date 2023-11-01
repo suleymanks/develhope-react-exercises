1e5c79945dafc6c725b76074aa7ae2fcbdbf4ea0
suleyman <suleymanks@live.com>
2023-11-01 17:20:24 +0300

    28

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Hello from "./components/Hello"
 import Message from "./components/Message"
 import Welcome from './components/Welcome';
 import AlertClock from './components/AlertClock';
+import Counter from './components/Counter';
 
 function App() {
 
 
   //27-EVENTS EXERCISE
   return (
-    <h1> 
+    <div> 
       <Message />
       <Welcome name={<strong>Suleyman!</strong>} />
       <AlertClock />
-    </h1>
+      <h1>
+        <Counter />
+      </h1>
+    </div>
   );
 }
 
