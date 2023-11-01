4c36bbb3cdc1eed694c584f501ac70c5bb382543
suleymanks <138375431+suleymanks@users.noreply.github.com>
2023-11-01 16:46:22 +0300

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,11 @@
 import YourAgeIs from "./YourAgeIs"
 
 const Welcome = (props) => {
+    const age = 22;
     return (
         <p>
             Welcome {props.name}
-            <YourAgeIs age={25} />
+            {18<age<65 && props.name="John" && <YourAgeIs age={25} />}
         </p>
     )
 }
