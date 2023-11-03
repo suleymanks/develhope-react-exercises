ab9a13168741004141d28816a0bff8f49cf40768
suleyman <suleymanks@live.com>
2023-11-03 18:41:50 +0300

    39

--- src/components/MouseClicker.jsx
+++ src/components/MouseClicker.jsx
@@ -3,7 +3,10 @@ export default function MouseClicker(props) {
     return (
         <>
             <button onClick={(e) => console.log(e.target.name)} name={props.name}>
-                <img onClick={(e)=> console.log(e.target.name)} src="https://www.w3schools.com/images/compatible_safari.png"/>
+                <img 
+                onClick={(e)=> {console.log(e.target.name); e.stopPropagation()}} 
+                src="https://www.w3schools.com/images/compatible_safari.png"
+                />
             </button>
         </>
     )
