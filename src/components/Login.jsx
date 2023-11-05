cc795b015072bfde2320f295ef9865a4de8d8210
suleyman <suleymanks@live.com>
2023-11-05 16:23:14 +0300

    45

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,42 +1,56 @@
 import { useState } from "react";
 
 export default function Login(props) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [checkbox, setCheckbox] = useState(false);
+    const [formData, setFormData] = useState({
+        username:"",
+        password:"",
+        checkbox:false,
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password, checkbox)
+        console.log(formData.username, formData.password, formData.checkbox)
+    }
+
+    function handleChange(e) {
+        const {name,value,type,checked} = e.target
+        setFormData((formData) => {
+            return {
+                ...formData,
+                [name] : type == 'checkbox' ? checked : value,
+            }
+        })
     }
 
     function onReset() {
-        setUsername("")
-        setPassword("")
-        setCheckbox(false)
+        setFormData({
+            username:"",
+            password:"",
+            checkbox:false
+        })
       }
 
     return (
         <form onSubmit={handleSubmit}>
-            <label >Username: </label>
+            <label>Username: </label>
             <input 
                 type="text" 
-                value={username} 
-                onChange={(e) => {setUsername(e.target.value)}}
-                name="name" />
+                value={formData.username} 
+                onChange={handleChange}
+                name="username" />
             <label >Password: </label>
             <input 
                 type="text" 
-                value={password} 
-                onChange={(e) => (setPassword(e.target.value))}
-                name="pass" />
+                value={formData.password} 
+                onChange={handleChange}
+                name="password" />
             Remember me ?
             <input 
                 type="checkbox" 
-                value={checkbox} 
-                onChange={(e)=> setCheckbox(!checkbox)}
-                name="remember" />
-            <button disabled={!username || !password} onClick={props.onLogin}>Login</button>
+                checked={formData.checkbox} 
+                onChange={handleChange}
+                name="checkbox" />
+            <button disabled={!formData.username || !formData.password} onClick={props.onLogin}>Login</button>
             <button onClick={onReset}>Reset</button>
         </form>
     )
