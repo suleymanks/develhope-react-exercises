ec76fcc20a35c43b75d4aa21446ff69a0c88285f
suleymanks <suleymanks@live.com>
2023-11-16 00:17:08 +0300

    69

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
 
   return (
-    <Container title={"its a Component Composition Exercise!"}>
+    <Container title={"its a some content that will be hidden!"}>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
--- src/container/Container.jsx
+++ src/container/Container.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react"
 
 export default function Container({children,title}) {
+    const [collapse, setCollapse] = useState(false)
+
     return (
         <div className="bg-white border-2 border-red-500">
-            {children}
-            <h1>{title}</h1>
+            <h1 onClick={()=> setCollapse(!collapse)} className="cursor-pointer">
+                {title}
+            </h1>
+            {collapse && children}
         </div>
     )
 }
