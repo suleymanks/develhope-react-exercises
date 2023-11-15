26a5c93985c1592c5ce00075bdb7508a6085e380
suleymanks <suleymanks@live.com>
2023-11-15 21:15:41 +0300

    67

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,17 @@
 import './App.css'
-import Welcome from './components/Welcome';
-import Counter from './components/Counter';
-import FocusableInput from './components/FocusableInput';
-import UncontrolledLogin from './components/UncontrolledLogin';
-import Login from './components/Login';
-import Clock from './components/Clock';
-import TodoList from './components/TodoList';
-import Colors from './components/Colors';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from './container/Container';
 
 
 function App() {
 
-  function onLogin() {
-    console.log("onlogin runs")
-  }
-
-  const colorsArray = [
-    {
-      name: "red",
-      id: 0,
-    },
-    {
-      name: "pink",
-      id: 1,
-    },
-    {
-      name: "yellow",
-      id: 2,
-    },
-    {
-      name: "blue",
-      id: 3,
-    },
-
-    ]
 
   return (
-    <div className='App'>
-      <h1 class="text-3xl font-bold underline">
+    <Container>
+      <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <Welcome />
-      <FocusableInput />
-      <Counter initial={0} incrementAmount={1} decrementAmount={1}/>
-      <Colors items={colorsArray}/>
-      <UncontrolledLogin onLogin={onLogin}/>
-      <Login onLogin={onLogin}/>
-      <Clock />
-      <TodoList />
-    </div>
+    </Container>
   );
 }
 
---
+++ src/container/Container.jsx
@@ -0,0 +1,8 @@
+
+export default function Container({children}) {
+    return (
+        <div className="bg-white border-2 border-red-500">
+            {children}
+        </div>
+    )
+}
