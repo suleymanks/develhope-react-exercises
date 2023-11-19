ad8ca4151f482b4968f7cdc9e58abcec3c3e1659
suleymanks <suleymanks@live.com>
2023-11-20 00:18:33 +0300

    84

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Welcome from './components/Welcome';
 import Counter from './components/Counter';
+import GitHubUser from './components/GitHubUser'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Welcome name={"Suleyman"} />} />
         <Route path='/counter' element={<Counter initial={3} incrementAmount={3} decrementAmount={5}/>} />
+        <Route path='/users/:username' element={<GitHubUser username={'suleymanks'}/>} />
       </Routes>
     </BrowserRouter>
   );
