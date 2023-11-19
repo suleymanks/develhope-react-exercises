94c35c3940208b2cffb4e44f3d55a45211700c2e
suleymanks <suleymanks@live.com>
2023-11-20 00:14:36 +0300

    83

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Welcome from './components/Welcome';
+import Counter from './components/Counter';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Welcome name={"Suleyman"} />} />
+        <Route path='/counter' element={<Counter initial={3} incrementAmount={3} decrementAmount={5}/>} />
       </Routes>
     </BrowserRouter>
   );
