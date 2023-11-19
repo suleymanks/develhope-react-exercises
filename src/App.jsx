e98c88d04ac237ab47c77aaae44cb64ff9cb0466
suleymanks <suleymanks@live.com>
2023-11-20 00:07:46 +0300

    82

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import FilteredList from './components/FilteredList';
+import Welcome from './components/Welcome';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const dataList = [
-    { id: 1, name: 'Suleyman', age: 25 },
-    { id: 2, name: 'Jale', age: 18 },
-    { id: 3, name: 'Burak', age: 30 },
-    { id: 4, name: 'Kazim', age: 17 },
-    
-  ];
 
   return (
-    <div>
-      <h1>FilteredList Example</h1>
-      <FilteredList list={dataList} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Welcome name={"Suleyman"} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,10 @@
 import '../index.css';
 
-export default function Welcome() {
+export default function Welcome({name}) {
     return (
         <div className='welcome'>
             <h1>React Exercise - Styling Components</h1>
-            <p>Welcome!</p>
+            <p>Welcome, {name}!</p>
         </div>
     )
 }
